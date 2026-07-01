ceaaf427ea1c70778b74b31a2e757f86a13585b5
Matias Valdivieso <mvaldivieso31@gmail.com>
2026-07-01 15:38:23 -0500

    resolcuion de uso desde local

--- app/config/app.php
+++ app/config/app.php
@@ -2,7 +2,7 @@
 
 return [
 	'name' => 'ISTS Ticket',
-	'url' => (string) env('APP_URL', '/istsTicket/public'),
+	'url' => (string) env('APP_URL', '/superarseTickets/public'),
 	'timezone' => 'America/Guayaquil',
 	'debug' => strtolower((string) env('APP_DEBUG', 'true')) === 'true',
 ];
